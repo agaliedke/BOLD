0e469626d1b236dd58fa7a947842530dee7f31cd
Bartosz Liedke <liedkeb@gmail.com>
2015-12-30 09:56:17 +0100

    - added image control - added counter control and functionality - added functionality for storing and changing image BitMap in a List

--- BOLD/MainWindow.xaml.cs
+++ BOLD/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,16 +24,75 @@ namespace BOLD
         public MainWindow()
         {
             InitializeComponent();
+            txtNum.Text = _numValue.ToString();
         }
-
+        // Menu items controlers
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        private void Open_Click(object sender, RoutedEventArgs e)
+        private void Add_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                image.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            _images.Add(image.Source);
+            NumValue++;
+        }
+        // List of images
+        private List<ImageSource> _images = new List<ImageSource>();
+        // controlers for image changer
+        private int _numValue = 0;
 
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                if (value < 0 && _images.Count==0)
+                    _numValue = 0;
+                else if (value < 1 && _images.Count>0)
+                    _numValue = 1;
+                else if (value > _images.Count)
+                    _numValue = _images.Count;
+                else
+                    _numValue = value;
+                txtNum.Text = _numValue.ToString();
+                if (_images.Count > 0)
+                {
+                    image.Source = _images[_numValue - 1];
+                   
+                }
+            }
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+            {
+                if (_numValue < 0)
+                    _numValue = 0;
+                else if (_numValue > _images.Count)
+                    _numValue = _images.Count;
+                txtNum.Text = _numValue.ToString();
+            }
         }
     }
+
 }
