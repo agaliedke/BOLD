dc77c5afc15c5e806a4c0f417f5e1be0c540a471
Bartosz Liedke <liedkeb@gmail.com>
2016-01-03 22:26:03 +0100

    Finished view functionality for resize

--- BOLD/MainWindow.xaml.cs
+++ BOLD/MainWindow.xaml.cs
@@ -30,90 +30,115 @@ namespace BOLD
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
+                // load image from file
                 ImageSlice slice = new ImageSlice(openFileDialog.FileName);
                 _numSlice = 1;
-                image.Source = slice.GetImage(_numSlice - 1);
+
+                // update text field
                 txtNum.Text = _numSlice.ToString();
 
-                //_images.Add(image.Source);
-                _image_data.Add(slice);
+                // insert new data image
+                _imageData.Add(slice);
 
+                // update combobox
                 ComboboxItem item = new ComboboxItem();
                 item.Text = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
                 item.Value = _numImage++;
                 fileNameBox.Items.Add(item);
                 fileNameBox.SelectedIndex = _numImage - 1;
 
+                // update Image
+                NumSlice = _numSlice;
+
             }
 
         }
         // List of images
-        private List<ImageSlice> _image_data = new List<ImageSlice>();
-        // controlers for image slice changer
-        private int _numSlice = 0;
+        private List<ImageSlice> _imageData = new List<ImageSlice>();
+        
         private int _numImage = 0;
-        public int NumValue
+
+        // slice changer controler
+        private int _numSlice = 0;
+        public int NumSlice
         {
             get { return _numSlice; }
             set
             {
-                if (_image_data.Count == 0)
+                if (_imageData.Count == 0)
                     return;
+
+                // check if slice is in range
                 if (value < 1 )
                     _numSlice = 1;
-                else if (value > _image_data[fileNameBox.SelectedIndex].zSize)
-                    _numSlice = _image_data[fileNameBox.SelectedIndex].zSize;
+                else if (value > _imageData[fileNameBox.SelectedIndex].zSize)
+                    _numSlice = _imageData[fileNameBox.SelectedIndex].zSize;
                 else
                     _numSlice = value;
+                // update text box
                 txtNum.Text = _numSlice.ToString();
-                if (_image_data.Count > 0)
-                {
-                    image.Source = _image_data[fileNameBox.SelectedIndex].GetImage(_numSlice-1);
-                }
+                // insert proper image
+                image.Source = _imageData[fileNameBox.SelectedIndex].GetImage(_numSlice - 1);
+                // if resize box is checked resize image
+                CheckedResize = resize.IsChecked.Value;
+
+            }
+        }
+
+        // controler for image resize checkbox
+        bool _checkedResize = false;
+        public bool CheckedResize
+        {
+            get { return _checkedResize; }
+            set
+            {
+                if (_imageData.Count == 0)
+                    return;
+                if (resize.IsChecked ?? true)
+                    image.Source = new CroppedBitmap(image.Source as BitmapSource, _imageData[fileNameBox.SelectedIndex].selection);
+                else
+                    image.Source = _imageData[fileNameBox.SelectedIndex].GetImage(_numSlice - 1);
             }
         }
 
         private void cmdUp_Click(object sender, RoutedEventArgs e)
         {
-            NumValue++;
+            NumSlice++;
         }
 
         private void cmdDown_Click(object sender, RoutedEventArgs e)
         {
-            NumValue--;
+            NumSlice--;
         }
 
         private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (txtNum == null)
-            {
                 return;
-            }
 
             if (int.TryParse(txtNum.Text, out _numSlice))
-            {
-                NumValue = _numSlice;
-            }
+                NumSlice = _numSlice;
 
 
         }
 
         private void fileName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (_numSlice > _image_data[fileNameBox.SelectedIndex].zSize)
-                NumValue = _numSlice = _image_data[fileNameBox.SelectedIndex].zSize;
+            if (_numSlice > _imageData[fileNameBox.SelectedIndex].zSize)
+                NumSlice = _numSlice = _imageData[fileNameBox.SelectedIndex].zSize;
             else
-                image.Source = _image_data[fileNameBox.SelectedIndex].GetImage(_numSlice - 1);
+                NumSlice = _numSlice;
+                //image.Source = _imageData[fileNameBox.SelectedIndex].GetImage(_numSlice - 1);
 
         }
 
         private void resize_Checked(object sender, RoutedEventArgs e)
         {
-            image.Source = new CroppedBitmap(image.Source as BitmapSource, _image_data[fileNameBox.SelectedIndex].selection);
+            CheckedResize = true;
         }
         private void resize_UnChecked(object sender, RoutedEventArgs e)
         {
-            image.Source = _image_data[fileNameBox.SelectedIndex].GetImage(_numSlice - 1);
+            CheckedResize = false;
         }
     }
 
